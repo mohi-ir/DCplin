3aa06eddb0161739d97d412aad29fa4a40240171
sina <sina.saeedi.edu@gmail.com>
2017-03-14 14:02:39 +0330

    notes list displayed in home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NoteService } from './providers/note.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [NoteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/note/note.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent implements OnInit {
+  title: string;
+  content: string;
+  tags: string[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
